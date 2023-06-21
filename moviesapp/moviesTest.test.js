da6de7abf3f902ddd9c396a0f449a6c88c4bcea5
Peter Zecca <pzecca94@gmail.com>
2023-06-21 16:46:31 -0400

    completed lab

--- moviesapp/moviesTest.test.js
+++ moviesapp/moviesTest.test.js
@@ -12,17 +12,17 @@ afterEach(async() => {
 });
 
 describe('Test the Movies App', () => {
-    // test('check and uncheck movie', async () => {
-    //     // await driver.get('http://localhost:3000/')
-    //     // await driver.findElement(By.id('add-movie-input')).sendKeys('The Godfather', Key.RETURN)
-    //     // await driver.findElement(By.css('input[type="checkbox"]')).click()
-    //     // await driver.sleep(4000)
-    //     // await driver.findElement(By.css('input[type="checkbox"]')).click()
-    //     // await driver.sleep(4000)
-    //     // // const checkbox = await driver.wait(until.elementLocated(By.css('#movies-list li input')), 1000)
-    //     // // expect(await checkbox)
+    test('check and uncheck movie', async () => {
+        await driver.get('http://localhost:3000/')
+        await driver.findElement(By.id('add-movie-input')).sendKeys('The Godfather', Key.RETURN)
+        await driver.findElement(By.css('input[type="checkbox"]')).click()
+        await driver.sleep(4000)
+        await driver.findElement(By.css('input[type="checkbox"]')).click()
+        await driver.sleep(4000)
+        const checkbox = await driver.wait(until.elementLocated(By.css('#movies-list li input')), 1000)
+        expect(await checkbox)
 
-    // })
+    })
     test ('delete movie', async () => {
         await driver.get('http://localhost:3000/')
         await driver.findElement(By.id('add-movie-input')).sendKeys('The Godfather', Key.RETURN)
@@ -33,6 +33,15 @@ describe('Test the Movies App', () => {
     test('notification displayed', async () => {
         await driver.get('http://localhost:3000/')
         await driver.findElement(By.id('add-movie-input')).sendKeys('The Godfather', Key.RETURN)
-        await driver.findElement(By.id('message'))
+        await driver.findElement(By.css('input[type="checkbox"]')).click()
+        const watched = await driver.wait(until.elementLocated(By.id('message')), 1000)
+        expect(await watched.getText()).toBe('Watched The Godfather')
+        await driver.findElement(By.css('input[type="checkbox"]')).click()
+        const addedBack = await driver.wait(until.elementLocated(By.id('message')), 1000)
+        expect(await addedBack.getText()).toBe('Added back The Godfather')
+        await driver.findElement(By.css('button[class="delete-btn"]')).click()
+        const deleted = await driver.wait(until.elementLocated(By.id('message')), 1000)
+        expect(await deleted.getText()).toBe('The Godfather deleted!')
+
     })
 })
